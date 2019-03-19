7e97c8d15a9c190a49a792fb5c16d9b400c39f4e
Роман Кузнецкий <r.kuzneckiy@geoscan.lan>
2019-03-19 18:04:01 +0300

    logo

--- conf.py
+++ conf.py
@@ -77,13 +77,6 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {'logo_only': True,}
-
-html_logo = "_static/_images/logo.png"
-html_favicon = "_static/_images/favicon.ico"
-html_show_sphinx = False
-
-
 def setup(app):
     app.add_stylesheet('style.css')
 
@@ -91,8 +84,11 @@ def setup(app):
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'logo_only': True,}
 
+html_logo = "_static/_images/logo.png"
+html_favicon = "_static/_images/favicon.ico"
+html_show_sphinx = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
