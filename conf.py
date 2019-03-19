6a98b3bf4a01bffa362b33ce71dd629dcd90cc56
Роман Кузнецкий <r.kuzneckiy@geoscan.lan>
2019-03-19 18:10:44 +0300

    no logo(

--- conf.py
+++ conf.py
@@ -84,11 +84,9 @@ def setup(app):
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'logo_only': True,}
+#html_theme_options = {'logo_only': True,}
 
-html_logo = "_static/_images/logo.png"
-html_favicon = "_static/_images/favicon.ico"
-html_show_sphinx = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
