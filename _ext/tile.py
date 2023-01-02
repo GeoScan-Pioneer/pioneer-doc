81ea74789f74536004e5365201121d8f016fb3cb
Dgtal1 <helloworld.danil@gmail.com>
2023-01-02 21:29:16 +0300

    Обновление раскрывающихся плашек - можно теперь не писать все элементы, и оно все равно будет выглядеть в прнципе красиво

--- _ext/tile.py
+++ _ext/tile.py
@@ -10,18 +10,43 @@ class tilenode(Element):
     pass
 
 def visit_tile(self, node):
-    template = """
-            <div class="tile">
-                <div class="image"> <img src="%(imgpath)s" alt="icon"> </div>
-                <div class="header"> <p>%(headertext)s</p> </div>
-                <div class="spacer"></div>
-                <hr noshade>
-                <div class="caption"> <p>%(captiontext)s</p> </div>
-                <div class="content">"""
+    template = '            <div class="tile'
 
-    self.body.append(template % {'imgpath': node['icon'],
-                                 'headertext': node['head'],
-                                 'captiontext': node['caption']})
+    if node['icon'] is None:
+        template += ' no-image'
+    if node['head'] is None or node['caption'] is None:
+        template += ' no-any-text'
+
+    template += '">'
+
+    if node['icon'] is not None:
+        ins = node['icon']
+        template += '\n'
+        template += f'                <div class="image"> <img src="{ins}" alt="icon"> </div>'
+    else:
+        template += '\n'
+        template += '                <div class="spacer">▶</div>'
+
+    if node['head'] is not None:
+        ins = node['head']
+        template += '\n'
+        template += f'                <div class="header"> <p>{ins}</p> </div>'
+
+    if node['head'] is not None and node['caption'] is not None:
+        template += '\n'
+        template += '                <div class="spacer"></div>'
+        template += '\n'
+        template += '                <hr noshade>'
+
+    if node['caption'] is not None:
+        ins = node['caption']
+        template += '\n'
+        template += f'                <div class="caption"> <p>{ins}</p> </div>'
+
+    template += '\n'
+    template += '                <div class="content">'
+
+    self.body.append(template)
 
 def depart_tile(self, node):
     self.body.append("""</div></div>""")
@@ -51,9 +76,9 @@ class TileDirective(Directive):
         # nested_parse_with_titles(self.state, rst, node)
         self.state.nested_parse(self.content, self.content_offset, node)
 
-        node['icon'] = self.options['icon'] if 'icon' in self.options else " "
-        node['head'] = self.options['head'] if 'head' in self.options else " "
-        node['caption'] = self.options['caption'] if 'caption' in self.options else " "
+        node['icon'] = self.options['icon'] if 'icon' in self.options else None
+        node['head'] = self.options['head'] if 'head' in self.options else None
+        node['caption'] = self.options['caption'] if 'caption' in self.options else None
         return [node]
 
 
--- _themes/sphinx_rtd_theme_upgraded/static/js/tiles.js
+++ _themes/sphinx_rtd_theme_upgraded/static/js/tiles.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     let tileHeight = getComputedStyle(document.documentElement).getPropertyValue('--tile-height');
     document.querySelectorAll(".tile").forEach(plate => {
-        plate.addEventListener('click', event => {
-            if (tileHeight.includes(plate.style.height) || plate.style.height == '') {
-                plate.style.height = plate.scrollHeight + 'px';
-                plate.classList.toggle('large');
+        var a = [
+            plate.getElementsByClassName('icon'),
+            plate.getElementsByClassName('header'),
+            plate.getElementsByClassName('caption'),
+            plate.getElementsByClassName('spacer')
+            ].forEach(aKey => {
+                if (aKey.length > 0) {
+                console.log(aKey)
+                aKey[0].addEventListener('click', event => {
+                    if (tileHeight.includes(plate.style.height) || plate.style.height == '') {
+                        plate.style.height = plate.scrollHeight + 'px';
+                        plate.classList.toggle('large');
+                    } else {
+                        plate.style.height = tileHeight;
+                        plate.classList.toggle('large');
+                    }
+                });
             }
-            else {
-                plate.style.height = tileHeight;
-                plate.classList.toggle('large');
-            }
-        });
+        })
         window.addEventListener('resize', event => {
             if (!tileHeight.includes(plate.style.height) && plate.style.height != '')
                 plate.style.height = plate.scrollHeight + 'px';
